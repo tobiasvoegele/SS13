9f756a00be8f2781c2a751066dbb731b3951a5af
Max Jando <max.jando@googlemail.com>
2013-05-30 21:52:26 +0300

    Update Employee.java

--- ADS/Übung 4/src/Employee.java
+++ ADS/Übung 4/src/Employee.java
@@ -1,14 +1,24 @@
+/**
+ * Max Jando 1312825 - Patrick Fruh 1314827
+ * Klasse Employee: Speichert einen Employee und verwaltet 
+ * verschiedene Methoden um einen Employee zu suchen 
+ */ 
+
 import java.text.SimpleDateFormat;
 
 public class Employee {
 
+
+	//Lokale Variablen-Deklaration
 	private int id;
 	private String firstName;
 	private String lastName;
 	private SimpleDateFormat birthday;
 	private String position;
-	Employee nextEmployee = null;
+	Employee nextEmployee = null; //Objektreferenz der Klasse Employee
 
+
+	//Konstruktor
 	Employee(int id, String firstName, String lastName,
 			SimpleDateFormat birthday, String position, Employee nextEmployee) {
 		this.id = id;
@@ -19,6 +29,7 @@ public class Employee {
 		this.nextEmployee = nextEmployee;
 	}
 
+	//Konstruktor
 	Employee(int id, String firstName, String lastName,
 			SimpleDateFormat birthday, String position) {
 		this.id = id;
@@ -27,17 +38,25 @@ public class Employee {
 		this.birthday = birthday;
 		this.position = position;
 	}
-
+	
+	
 	public boolean equalsID(int id) {
+	/**
+	 * Suchmethode, die bei übergebener ID-Variable true liefert
+	 */
 		return (this.id == id);
 	}
 
 	public boolean equalsLastName(String lastName) {
+	/**
+	 * Suchmethode, die bei übergebener LastName-Variable true liefert
+	 */
 		return this.lastName.equals(lastName);
 	}
 
 	@Override
 	public String toString() {
+		//toString Methode der Klasse Employee überschrieben um Ausgabe zu vereinfachen
 		return "Employee [id=" + id + ", firstName=" + firstName
 				+ ", lastName=" + lastName + ", birthday="
 				+ birthday.toLocalizedPattern() + ", position=" + position
